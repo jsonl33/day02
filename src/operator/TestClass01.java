73f9c252646c92e7441edb3b9243292b1509cb71
testuser <testuser@test.com>
2023-05-18 17:59:03 +0900

    quiz2

---
+++ src/operator/TestClass01.java
@@ -0,0 +1,17 @@
+package operator;
+
+import java.util.Scanner;
+
+public class TestClass01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		System.out.println("수 입력");
+		num = sc.nextInt();
+		String s = (num%2==0)?"짝수":"홀수";
+		System.out.println(num+"는 "+s);
+		
+		String th = (num%3==0)?"3의배수":"3의배수가 아님";
+		System.out.println(num+"는 "+th);
+	}
+}
